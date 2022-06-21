4474689fecf70161f8d2cb5794a9509416b88b78
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-06-21 09:48:17 -0300

    add observações sobre uso typeof

--- desafio02.js
+++ desafio02.js
@@ -11,3 +11,16 @@ console.log(myFunction(false)); // boolean
 console.log(myFunction({})); // object
 console.log(myFunction('Hello World')); // string
 console.log(myFunction(['Hello Word'])); // object
+
+// obs: typeof tipo number e object temos de comparar se também não é um NaN e para object se não é null.
+//ex:
+
+let numero = Number('teste');
+console.log(numero); // NaN
+console.log(typeof numero); // number
+console.log(typeof numero === 'number' && !isNaN(numero)); // false
+
+let user = null;
+console.log(user); // null
+console.log(typeof user); // object
+console.log(typeof user === 'object' && user !== null); //false
