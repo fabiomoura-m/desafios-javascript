8a56a7e015cd12d679eebc01e7a2183729cce4c5
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-07-05 20:45:07 -0300

    add desafio 15

---
+++ desafio15.js
@@ -0,0 +1,11 @@
+/*
+Escreva uma função que recebe dois parâmetros(a,b), sendo a um objeto e b uma string.
+Retorne true se o objeto a conter a propriedade/chave com o nome passado em b.
+Caso contratio, retorne false
+*/
+function myFunction(a, b) {
+  return Object.keys(a).some(item => item === b);
+}
+console.log(myFunction({ a: 1, b: 2, c: 3 }, 'b')); // true
+console.log(myFunction({ x: 'a', y: 'b', z: 'c' }, 'a')); // false
+console.log(myFunction({ x: 'a', y: 'b', z: 'c' }, 'z')); // true
