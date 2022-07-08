2987a77cab75afc8cfc269d9e898b43261edd2e4
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-07-07 22:52:30 -0300

    add outra resolução do desafio 15

--- desafio15.js
+++ desafio15.js
@@ -4,7 +4,8 @@ Retorne true se o objeto a conter a propriedade/chave com o nome passado em b.
 Caso contratio, retorne false
 */
 function myFunction(a, b) {
-  return Object.keys(a).some(item => item === b);
+  // return Object.keys(a).some(item => item === b);
+  return Object.keys(a).includes(b);
 }
 console.log(myFunction({ a: 1, b: 2, c: 3 }, 'b')); // true
 console.log(myFunction({ x: 'a', y: 'b', z: 'c' }, 'a')); // false
